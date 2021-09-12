f39db139bc8b8e69d545e15dff67aae6c65bf831
syh8088 <syh8088@gmail.com>
2021-09-12 21:12:25 +0900

    설정 값 암호화 적용
    설정 값 암호화 적용

--- server-api/src/main/java/com/serverapi/ServerApiController.java
+++ server-api/src/main/java/com/serverapi/ServerApiController.java
@@ -1,10 +1,12 @@
 package com.serverapi;
 
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Value;
 
 @RestController
+@RefreshScope
 public class ServerApiController {
 
     @Value("${docfriends.tel-numbers.jeonneung.block}")
@@ -12,6 +14,9 @@ public class ServerApiController {
 
     @Value("${docfriends.tel-numbers.bit-computer.block}")
     private String bitComputerBlockNumber;
+
+    @Value("${spring.datasource.password}")
+    private String password;
 
     @GetMapping("jeonneung-block-number")
     public String getJeonneungBlockNumber() {
@@ -21,5 +26,10 @@ public class ServerApiController {
     @GetMapping("bit-computer-block-number")
     public String getBitComputerBlockNumber() {
         return String.format("Server API bit-computer block number = " + bitComputerBlockNumber);
+    }
+
+    @GetMapping("password")
+    public String getPassword() {
+        return String.format("Database Password = " + password);
     }
 }
